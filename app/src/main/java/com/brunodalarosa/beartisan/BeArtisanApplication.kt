4d37bbdd078dad0da4f676799dbfd156e8f40f84
Bruno Cesar Puli Dala Rosa <obrunodalarosa@gmail.com>
2020-09-09 22:54:29 -0300

    Added Hilt and Room in the project!

---
+++ app/src/main/java/com/brunodalarosa/beartisan/BeArtisanApplication.kt
@@ -0,0 +1,7 @@
+package com.brunodalarosa.beartisan
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BeArtisanApplication : Application()
---
+++ app/src/main/java/com/brunodalarosa/beartisan/di/DatabaseModule.kt
@@ -0,0 +1,22 @@
+package com.brunodalarosa.beartisan.di
+
+import android.content.Context
+import androidx.room.Room
+import com.brunodalarosa.beartisan.data.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@InstallIn(ApplicationComponent::class)
+@Module
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(context, AppDatabase::class.java, "be-artisan.db").build()
+    }
+}
